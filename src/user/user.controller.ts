c92b9a34e1ccf6b90c06534b6fc71e19b174c7e4
Yuriy Kvartsianiy <liparg104@gmail.com>
2025-03-29 12:39:13 +0200

    feature: implement user profile route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,15 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards';
+import { UserEntity } from './user.entity';
+import { User } from '@/common/decorators/user.decorator';
 
 @Controller('users')
+@UseGuards(JwtAuthGuard)
 export class UserController {
   constructor() {}
 
-  @UseGuards(JwtAuthGuard)
   @Get('profile')
-  getProfile() {
-    return 'profile';
+  getProfile(@User() user: UserEntity) {
+    return user;
   }
 }
