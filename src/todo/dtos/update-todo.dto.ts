dba06835dc7971758802f612e3295b861adcfa7e
Yuriy Kvartsianiy <liparg104@gmail.com>
2025-03-31 19:51:17 +0300

    feature: replace is_completed with status field in todos entity and update todo dto

--- src/todo/dtos/update-todo.dto.ts
+++ src/todo/dtos/update-todo.dto.ts
@@ -1,10 +1,13 @@
-import { IsBoolean, IsString, MinLength } from 'class-validator';
+import { IsEnum, IsOptional, IsString, MinLength } from 'class-validator';
+
+import { TodoStatusEnum } from '../constants';
 
 export class UpdateTodoDto {
   @IsString()
   @MinLength(3)
   title: string;
 
-  @IsBoolean()
-  isCompleted: boolean;
+  @IsOptional()
+  @IsEnum(TodoStatusEnum)
+  status?: TodoStatusEnum;
 }
