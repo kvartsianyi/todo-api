91604d912ef023218624a9f194d1df393f0826d7
Yuriy Kvartsianiy <liparg104@gmail.com>
2025-03-31 19:51:17 +0300

    feature: replace is_completed with status field in todos entity and update todo dto

--- src/todo/todo.entity.ts
+++ src/todo/todo.entity.ts
@@ -2,14 +2,19 @@ import { Entity, Column, ManyToOne, JoinColumn } from 'typeorm';
 
 import { BaseEntity } from 'src/database/entities/base.entity';
 import { UserEntity } from '@/user/user.entity';
+import { TodoStatusEnum } from './constants';
 
 @Entity('todo')
 export class TodoEntity extends BaseEntity {
   @Column()
   title: string;
 
-  @Column({ default: false, name: 'is_completed' })
-  isCompleted: boolean;
+  @Column({
+    type: 'enum',
+    enum: TodoStatusEnum,
+    default: TodoStatusEnum.TODO,
+  })
+  status: TodoStatusEnum;
 
   @Column({ name: 'user_id' })
   userId: number;
