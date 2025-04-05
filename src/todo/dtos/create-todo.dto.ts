e2e43e34c1c701333707b55704d3cf1a76fbca8e
Yuriy Kvartsianiy <liparg104@gmail.com>
2025-04-06 00:08:43 +0300

    feature: add description and dueAt fields to todo

--- src/todo/dtos/create-todo.dto.ts
+++ src/todo/dtos/create-todo.dto.ts
@@ -1,11 +1,27 @@
-import { IsString, Length } from 'class-validator';
+import { IsDate, IsOptional, IsString, Length } from 'class-validator';
 
-import { TITLE_MAX_LENGTH, TITLE_MIN_LENGTH } from '../constants';
-import { Transform } from 'class-transformer';
+import {
+  DESCRIPTION_MAX_LENGTH,
+  DESCRIPTION_MIN_LENGTH,
+  TITLE_MAX_LENGTH,
+  TITLE_MIN_LENGTH,
+} from '../constants';
+import { Transform, Type } from 'class-transformer';
 
 export class CreateTodoDto {
   @IsString()
   @Length(TITLE_MIN_LENGTH, TITLE_MAX_LENGTH)
   @Transform(({ value }) => value.trim())
   public readonly title: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(DESCRIPTION_MIN_LENGTH, DESCRIPTION_MAX_LENGTH)
+  @Transform(({ value }) => value.trim())
+  public readonly description?: string;
+
+  @IsOptional()
+  @IsDate()
+  @Type(() => Date)
+  public readonly dueAt?: Date;
 }
--- src/todo/dtos/update-todo.dto.ts
+++ src/todo/dtos/update-todo.dto.ts
@@ -1,10 +1,12 @@
-import { IsEnum, IsOptional, IsString, Length } from 'class-validator';
-import { Transform } from 'class-transformer';
+import { IsDate, IsEnum, IsOptional, IsString, Length } from 'class-validator';
+import { Transform, Type } from 'class-transformer';
 
 import {
   TodoStatusEnum,
   TITLE_MIN_LENGTH,
   TITLE_MAX_LENGTH,
+  DESCRIPTION_MIN_LENGTH,
+  DESCRIPTION_MAX_LENGTH,
 } from '../constants';
 
 export class UpdateTodoDto {
@@ -17,4 +19,15 @@ export class UpdateTodoDto {
   @IsOptional()
   @IsEnum(TodoStatusEnum)
   public readonly status?: TodoStatusEnum;
+
+  @IsOptional()
+  @IsString()
+  @Length(DESCRIPTION_MIN_LENGTH, DESCRIPTION_MAX_LENGTH)
+  @Transform(({ value }) => value.trim())
+  public readonly description?: string;
+
+  @IsOptional()
+  @IsDate()
+  @Type(() => Date)
+  public readonly dueAt?: Date;
 }
