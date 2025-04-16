fffc5317e8aa2e0e3aad6cf8c944a754af7719d7
Yuriy Kvartsianiy <liparg104@gmail.com>
2025-04-16 17:59:07 +0300

    feature: add swagger docs

---
+++ src/common/swagger/constants/config.constant.ts
@@ -0,0 +1,7 @@
+export const DOCUMENT_CONFIG = {
+  TITLE: 'The todos API',
+  DESCRIPTION: 'API Documentation for todos. Todo it!',
+  VERSION: '0.1',
+  TAG: 'todo-api',
+  PATH: 'api-docs',
+} as const;
